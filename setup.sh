bbc2deeadb8d1d83303b7f535d3ad6829c201231
Claudio Freitas <claudiofreitas3@gmail.com>
2021-09-17 20:52:26 +0900

    Create setup to install homebrew

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# -- Homebrew
+
+## Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## Opt out of analytics and display state
+brew analytics off
+brew analytics state
+
